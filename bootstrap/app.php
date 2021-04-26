e0f971c2eeb679df98371201e05167ee62f1cbf2
Rodrigo Karwinski <rodrigo.orth@hotmail.com>
2021-04-25 22:28:54 -0300

    ADD: controle de OPTIONS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -83,7 +83,7 @@ $app->configure('app');
 $app->middleware([
     App\Http\Middleware\CorsMiddleware::class 
 ]);
-
+ 
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
